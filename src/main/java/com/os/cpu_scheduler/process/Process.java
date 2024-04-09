787233dc77eeaa13813a7f570c7c0c229647a0d3
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-04-09 18:53:32 +0200

    added docs

--- src/main/java/com/os/cpu_scheduler/process/Process.java
+++ src/main/java/com/os/cpu_scheduler/process/Process.java
@@ -1,7 +1,11 @@
 package com.os.cpu_scheduler.process;
 
+/**
+ * The Process class represents a process entity with various attributes
+ * such as arrival time, burst time, name, remaining time, etc.
+ */
 public class Process {
-    // process elements
+    // process datafields
     private int arrivalTime;
     private int burstTime;
     private String name;
@@ -11,14 +15,16 @@ public class Process {
     private int turnaroundTime = 0;
     private int priority = 1;
     private static int counter;
-
-    public int getId() {
-        return id - 2;
-    }
-
     private int id;
 
-    // constructor
+    /**
+     * Constructs a process with the specified arrival time, burst time, and name.
+     * It also gives id to the process.
+     * 
+     * @param arrivalTime The arrival time of the process.
+     * @param burstTime   The burst time of the process.
+     * @param name        The name of the process.
+     */
     public Process(int arrivalTime, int burstTime, String name) {
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
@@ -27,7 +33,17 @@ public class Process {
         this.id = counter;
         counter++;
     }
-    public Process(int arrivalTime, int burstTime, String name , int priority) {
+
+    /**
+     * Constructs a process with the specified arrival time, burst time, name, and
+     * priority.
+     * 
+     * @param arrivalTime The arrival time of the process.
+     * @param burstTime   The burst time of the process.
+     * @param name        The name of the process.
+     * @param priority    The priority of the process.
+     */
+    public Process(int arrivalTime, int burstTime, String name, int priority) {
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.name = name;
@@ -37,6 +53,11 @@ public class Process {
         counter++;
     }
 
+    /**
+     * Constructs a process with the same attributes as the specified process.
+     * 
+     * @param p The process to deep copy.
+     */
     public Process(Process p) {
         this.name = new String(p.name);
         this.burstTime = p.burstTime;
@@ -47,38 +68,103 @@ public class Process {
         counter++;
     }
 
-    // some getters&setters
+    /**
+     * Gets the remaining time of the process.
+     * 
+     * @return The remaining time of the process.
+     */
     public int getRemainingTime() {
         return remainingTime;
     }
+
+    /**
+     * Gets the arrival time of the process.
+     * 
+     * @return The arrival time of the process.
+     */
     public int getArrivalTime() {
         return arrivalTime;
     }
+
+    /**
+     * Gets the burst time of the process.
+     * 
+     * @return The burst time of the process.
+     */
     public int getBurstTime() {
         return burstTime;
     }
+
+    /**
+     * Gets the priority of the process.
+     * 
+     * @return The priority of the process.
+     */
     public int getPriority() {
         return priority;
     }
+
+    /**
+     * Gets the waiting time of the process.
+     * 
+     * @return The waiting time of the process.
+     */
     public int getWaitingTime() {
         return waitingTime;
     }
+
+    /**
+     * Gets the turnaround time of the process.
+     * 
+     * @return The turnaround time of the process.
+     */
     public int getTurnaroundTime() {
         return turnaroundTime;
     }
+
+    /**
+     * Gets the name of the process.
+     * 
+     * @return The name of the process.
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Decrements the remaining time of the process by 1.
+     */
     public void decRemainingTime() {
         this.remainingTime -= 1;
     }
-    
+
+    /**
+     * Increments the waiting time of the process by 1.
+     */
     public void incWaitingTime() {
         this.waitingTime += 1;
     }
-    
+
+    /**
+     * Increments the turnaround time of the process by 1.
+     */
     public void incTurnaroundTime() {
         this.turnaroundTime += 1;
+    }
+
+    /**
+     * Gets the ID of the process.
+     * @return the id of the current process
+     */
+    public int getId() {
+        return id - 2;
+    }
+
+    /**
+     * Resets the counter used for assigning IDs to processes.
+     */
+    public static void resetCounter() {
+        counter = 0;
     }
 
     @Override
@@ -89,10 +175,6 @@ public class Process {
                 ", burstTime=" + burstTime +
                 ", remainingTime=" + remainingTime +
                 '}';
-    }
-
-    public static void resetCounter() {
-        counter = 0;
     }
 
 }
--- src/main/java/com/os/cpu_scheduler/process/ProcessList.java
+++ src/main/java/com/os/cpu_scheduler/process/ProcessList.java
@@ -1,74 +1,114 @@
 package com.os.cpu_scheduler.process;
+
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class ProcessList{
+/**
+ * The ProcessList class represents a list of processes and provides various methods to manipulate and analyze them.
+ */
+public class ProcessList {
     private ArrayList<Process> processes;
     private int totalRemainingTime = 0;
     private int clockCounter = 0;
-    
 
-    private void sortTheArray(){
-        Comparator<Process> comparator = Comparator.comparingInt(Process::getArrivalTime);
-        Collections.sort(processes, comparator);
-    }
-
+    /**
+     * Constructs an empty ProcessList.
+     */
     public ProcessList() {
         this.processes = new ArrayList<Process>();
     }
 
+    /**
+     * Retrieves the list of processes.
+     * @return The list of processes.
+     */
     public ArrayList<Process> getProcesses() {
         return processes;
     }
+
+    /**
+     * Retrieves the process at the specified index.
+     * @param i The index of the process to retrieve.
+     * @return The process at the specified index.
+     */
     public Process get(int i){
         return processes.get(i);
     }
+
+    /**
+     * Retrieves the number of processes in the list.
+     * @return The number of processes in the list.
+     */
     public int length(){
         return processes.size();
     }
 
+    /**
+     * Retrieves the total remaining time of all processes in the list.
+     * @return The total remaining time of all processes.
+     */
     public int getTotalRemainingTime() {
         return totalRemainingTime;
     }
 
+    /**
+     * Retrieves the clock counter.
+     * @return The clock counter.
+     */
     public int getClockCounter() {
         return clockCounter;
     }
 
+    /**
+     * Increments the clock counter by 1.
+     */
     public void incClockCounter() {
         this.clockCounter += 1;
     }
 
+    /**
+     * Adds a process to the list, sorting the list based on arrival time.
+     * @param process The process to add to the list.
+     */
     public void addProcess(Process process){
         processes.add(process);
         sortTheArray();
         totalRemainingTime += process.getRemainingTime();
     }
 
+    /**
+     * Checks if all processes in the list have finished execution.
+     * @return True if all processes have finished, otherwise false.
+     */
     public boolean isProcessesFinished() {
         boolean finish = true;
-        for(int i = 0; i < processes.size(); i++)
-            {
-                if(processes.get(i).getRemainingTime() > 0)
-                {
-                    finish = false;
-                    break;
-                }
-
+        for(int i = 0; i < processes.size(); i++) {
+            if(processes.get(i).getRemainingTime() > 0) {
+                finish = false;
+                break;
             }
+        }
         return finish;
     }
 
-
+    /**
+     * Calculates the average waiting time of all processes in the list.
+     * @return The average waiting time of all processes.
+     */
     public double avgWaitingTime(){
         double avg = 0;
         for (Process process : processes) {
             avg += process.getWaitingTime();
         }
         return Math.round((avg/processes.size()) *100)/(100.0);
-    }    
+    }
+
+    /**
+     * Calculates the average turnaround time of all processes in the list.
+     * @return The average turnaround time of all processes.
+     */
     public double avgTurnAroundTime(){
         double avg = 0;
         for (Process process : processes) {
@@ -77,11 +117,20 @@ public class ProcessList{
         return Math.round((avg/processes.size()) *100)/(100.0);
     }
 
+    /**
+     * Prints the arrival times of all processes in the list.
+     */
     public void printTheArray(){
         for (int i = 0; i < processes.size() ; i++) {
             System.out.println(processes.get(i).getArrivalTime());
         }
     }
- 
-}
 
+    /**
+     * Sorts the list of processes based on their arrival times.
+     */
+    private void sortTheArray(){
+        Comparator<Process> comparator = Comparator.comparingInt(Process::getArrivalTime);
+        Collections.sort(processes, comparator);
+    }
+}
