5978538ef23a60eab2c7fc58f929a361b93bc1f5
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-04-09 19:27:40 +0200

    added docs to InitController.java

--- src/main/java/com/os/cpu_scheduler/controller/InitController.java
+++ src/main/java/com/os/cpu_scheduler/controller/InitController.java
@@ -27,7 +27,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 public class InitController implements Initializable {
     private double offsetX;
     private double offsetY;
@@ -43,7 +42,6 @@ public class InitController implements Initializable {
 
     private Stage stage;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Font font = Font.loadFont(getClass().getResourceAsStream("/com/os/cpu_scheduler/font/LCD-BOLD.TTF"), 48);
@@ -55,14 +53,19 @@ public class InitController implements Initializable {
             System.out.println("Thread interrupted");
         }
 
-        playSound(InitController.class.getResource("/com/os/cpu_scheduler/audio/typing.wav").getPath().replaceAll("%20", " "));
+        playSound(InitController.class.getResource("/com/os/cpu_scheduler/audio/typing.wav").getPath().replaceAll("%20",
+                " "));
         pandaFloat();
         writeText();
         arcProgress();
     }
 
-
-    public static void playSound(String filePath) {
+    /**
+     * Plays Sound passed as path
+     * 
+     * @param filePath path of sound file
+     */
+    private static void playSound(String filePath) {
         try {
             File file = new File(filePath);
             if (file == null) {
@@ -77,15 +80,20 @@ public class InitController implements Initializable {
         }
     }
 
+    /**
+     * Plays arc progress animation
+     */
     private void arcProgress() {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(arc.lengthProperty(), -60)),
-                new KeyFrame(Duration.millis(7000), new KeyValue(arc.lengthProperty(), 360))
-        );
+                new KeyFrame(Duration.millis(7000), new KeyValue(arc.lengthProperty(), 360)));
 
         timeline.play();
     }
 
+    /**
+     * Plays animation of panda image
+     */
     private void pandaFloat() {
         TranslateTransition floating = new TranslateTransition(Duration.seconds(2), panda);
         floating.setFromY(0);
@@ -96,6 +104,10 @@ public class InitController implements Initializable {
         floating.play();
     }
 
+    /**
+     * Writes first welcome text "Welcome to Panda Scheduler" and deletes it letter
+     * by letter
+     */
     private void writeText() {
         String text = "Welcome to Panda Scheduler";
         AtomicInteger current = new AtomicInteger();
@@ -107,9 +119,7 @@ public class InitController implements Initializable {
                                 welcome.setText(welcome.getText() + text.charAt(current.get()));
                                 current.getAndIncrement();
                             }
-                        }
-                )
-        );
+                        }));
 
         timeline.setOnFinished(event -> {
             current.getAndIncrement();
@@ -123,9 +133,7 @@ public class InitController implements Initializable {
                                 if (current.get() < text.length()) {
                                     welcome.setText(welcome.getText().substring(0, current.get()));
                                 }
-                            }
-                    )
-            );
+                            }));
             timeline2.setCycleCount(text.length() + 3);
             timeline2.play();
             timeline2.setOnFinished(e -> writeText2());
@@ -135,6 +143,9 @@ public class InitController implements Initializable {
         timeline.play();
     }
 
+    /**
+     * Writes second welcome text "Let's Start" and deletes it letter by letter
+     */
     private void writeText2() {
         String text = "Let's Start";
         AtomicInteger current = new AtomicInteger();
@@ -146,9 +157,7 @@ public class InitController implements Initializable {
                                 welcome.setText(welcome.getText() + text.charAt(current.get()));
                                 current.getAndIncrement();
                             }
-                        }
-                )
-        );
+                        }));
 
         timeline.setOnFinished(event -> {
             current.getAndIncrement();
@@ -162,9 +171,7 @@ public class InitController implements Initializable {
                                 if (current.get() < text.length() && current.get() >= 0) {
                                     welcome.setText(welcome.getText().substring(0, current.get()));
                                 }
-                            }
-                    )
-            );
+                            }));
             timeline2.setCycleCount(text.length() + 10);
             timeline2.play();
             timeline2.setOnFinished(e -> {
@@ -186,34 +193,61 @@ public class InitController implements Initializable {
         timeline.play();
     }
 
+    /**
+     * Minimizes the window associated with the specified MouseEvent.
+     * 
+     * @param mouseEvent The MouseEvent triggering the window minimization.
+     */
     public void minimize(MouseEvent mouseEvent) {
+        // Retrieves the stage containing the window
         Stage stage = ((Stage) ((MFXFontIcon) mouseEvent.getSource()).getScene().getWindow());
+
+        // Minimizes the window
         stage.setIconified(true);
     }
 
     public void maximize() {
-//        Stage stage = ((Stage)((MFXFontIcon)mouseEvent.getSource()).getScene().getWindow());
-//        stage.setMaximized(!stage.isMaximized());
-//        if (stage.isMaximized()) {
-//            window.setWidth(stage.getWidth());
-//            window.setHeight(stage.getHeight());
-//        } else {
-//            window.setHeight(720);
-//            window.setWidth(1280);
-//        }
+        // Stage stage =
+        // ((Stage)((MFXFontIcon)mouseEvent.getSource()).getScene().getWindow());
+        // stage.setMaximized(!stage.isMaximized());
+        // if (stage.isMaximized()) {
+        // window.setWidth(stage.getWidth());
+        // window.setHeight(stage.getHeight());
+        // } else {
+        // window.setHeight(720);
+        // window.setWidth(1280);
+        // }
     }
 
+    /**
+     * Close the program on clicking exit button
+     */
     public void exit() {
         System.exit(0);
     }
 
+    /**
+     * Moves the window based on the specified MouseEvent.
+     * 
+     * @param mouseEvent The MouseEvent triggering the window movement.
+     */
     public void moveWindow(MouseEvent mouseEvent) {
+        // Retrieves the stage containing the window
         Stage stage = ((Stage) ((StackPane) mouseEvent.getSource()).getScene().getWindow());
+
+        // Calculates the new window position based on mouse movement and offsets
         stage.setX(mouseEvent.getScreenX() - offsetX);
         stage.setY(mouseEvent.getScreenY() - offsetY);
     }
 
+    /**
+     * Retrieves the X and Y offsets for window movement based on the specified
+     * MouseEvent.
+     * 
+     * @param mouseEvent The MouseEvent providing the offsets.
+     */
     public void getXYOffsets(MouseEvent mouseEvent) {
+        // Stores the X and Y offsets for window movement
         offsetX = mouseEvent.getSceneX();
         offsetY = mouseEvent.getSceneY();
     }
